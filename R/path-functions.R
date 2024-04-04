b0e59f1ea527b58504ee37d875eda29593421a04
emilio-berti <emilio.berti90@gmail.com>
2024-04-04 14:59:09 +0200

    coordinate fix in parthian_path

--- R/path-functions.R
+++ R/path-functions.R
@@ -31,7 +31,7 @@ parthian_path <- function(g, r, from = NULL, to = NULL) {
   
   # least cost path ----
   if (is(from, "SpatVector")) {
-    xy <- extract(r, rbind(from, to), cells = TRUE)
+    xy <- geom(rbind(from, to))[, c("x", "y")]
     from <- xy[["cell"]][1]
     to <- xy[["cell"]][2]
   }
